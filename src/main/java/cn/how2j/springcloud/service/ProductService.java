dc767c916a535faf76740141a1e01cab9520acf6
Mei-Jun-Tao <1613946868@qq.com>
2021-03-06 21:54:28 +0800

    test

---
+++ src/main/java/cn/how2j/springcloud/service/ProductService.java
@@ -0,0 +1,19 @@
+package cn.how2j.springcloud.service;
+
+import cn.how2j.springcloud.client.ProductClientFeign;
+import cn.how2j.springcloud.pojo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductClientFeign pcf;
+
+    public List<Product> listProduct(){
+        return pcf.listProducts();
+    }
+}
